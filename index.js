795be5701fce4954f3d8f8423ac39951eecd63cf
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-02-20 13:35:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var a=prompt("Welcome to Team Gryffindor's project on Nikola Tesla! Type S, to see the slides. Type W, to see the HTML project.");
+
+    if(a=="S") {
+        window.location.href="https://docs.google.com/presentation/d/1vJkUffVP4Ri_Dy_37G2vb9Su5uTdmMlo9aJutUfo2ew/edit#slide=id.g1159e141716_0_1066"
+    }
+
+    if(a=="W") {
+        window.location.href="content.html"
+    }
